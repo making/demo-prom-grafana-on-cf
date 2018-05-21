19d7c6c771acd7474b0889f6067a640115310904
Toshiaki Maki <tmaki@pivotal.io>
2018-05-21 13:51:39 +0900

    Update

--- prometheus1/cf-push.sh
+++ prometheus1/cf-push.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Push Promtheus1
-cf push prometheus1 -b binary_buildpack -c './prometheus -web.listen-address=:8080 -config.file=./prometheus-cf.yml' -m 128m --random-route -p prometheus-1.8.2.linux-amd64
+cf push -f manifest-prometheus1.yml
 cf map-route prometheus1 apps.internal -n prometheus1
 cf add-network-policy prometheus1 --destination-app demo-micrometer --protocol tcp --port 8080
 cf add-network-policy prometheus1 --destination-app tweet-micrometer --protocol tcp --port 8080
 
 # Push Grafana5
-cf push grafana5-for-prom1 -b binary_buildpack -c './bin/grafana-server -config=./conf/grafana-cf.ini' -m 128m  --random-route -p grafana-5.1.2
+cf push -f manifest-grafana5-for-prom1.yml
 cf add-network-policy grafana5-for-prom1 --destination-app prometheus1 --protocol tcp --port 8080
--- prometheus2/cf-push.sh
+++ prometheus2/cf-push.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Push Prometheus2
-cf push prometheus2 -b binary_buildpack -c './prometheus --web.listen-address=:8080 --config.file=./prometheus-cf.yml' -m 128m --random-route -p prometheus-2.2.1.linux-amd64
+cf push -f manifest-prometheus2.yml
 cf map-route prometheus2 apps.internal -n prometheus2
 cf add-network-policy prometheus2 --destination-app demo-micrometer --protocol tcp --port 8080
 cf add-network-policy prometheus2 --destination-app tweet-micrometer --protocol tcp --port 8080
 
 # Push Grafana5
-cf push grafana5-for-prom2 -b binary_buildpack -c './bin/grafana-server -config=./conf/grafana-cf.ini' -m 128m  --random-route -p grafana-5.1.2
+cf push -f manifest-grafana5-for-prom2.yml
 cf add-network-policy grafana5-for-prom2 --destination-app prometheus2 --protocol tcp --port 8080
